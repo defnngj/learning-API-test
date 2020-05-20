9ba02a9426065fd59e8a7b18fc6a1b1d1108290e
fnngj <fnngj@126.com>
2020-05-21 00:14:17 +0800

    chat robot

---
+++ chat_robot/chat_server.py
@@ -0,0 +1,50 @@
+import sanic
+import httpx
+from sanic import Sanic
+from sanic.response import json
+from sanic.websocket import WebSocketProtocol
+from sanic.exceptions import NotFound
+from sanic.response import html
+from jinja2 import Environment, PackageLoader
+
+
+env = Environment(loader=PackageLoader('app', 'templates'))
+
+app = Sanic(__name__)
+
+
+@app.route('/')
+async def index(request):
+    """
+    聊天页面
+    """
+    template = env.get_template('index.html')
+    html_content = template.render(title='聊天机器人')
+    return html(html_content)
+
+
+@app.websocket('/chat')
+async def chat(request, ws):
+    """
+    处理聊天信息，并返回消息
+    :param request:
+    :param ws:
+    :return:
+    """
+    while True:
+        user_msg = await ws.recv()
+        print('Received: ' + user_msg)
+        intelligence_data = {"key": "free", "appid": 0, "msg": user_msg}
+        r = httpx.get("http://api.qingyunke.com/api.php", params=intelligence_data)
+        chat_msg = r.json()["content"]
+        print('Sending: ' + chat_msg)
+        await ws.send(chat_msg)
+
+
+if __name__ == "__main__":
+    app.error_handler.add(
+        NotFound,
+        lambda r, e: sanic.response.empty(status=404)
+    )
+    app.run(host="192.168.0.7", port=8000, protocol=WebSocketProtocol, debug=True)
+
