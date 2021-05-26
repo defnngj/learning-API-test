ee3cdfcbb0ceec5a93125a416c7688d69ecf7b3f
defnngj <defnngj@gmail.com>
2021-05-27 01:14:16 +0800

    feat: fastapi

---
+++ fastapi_sample/README.md
@@ -0,0 +1,17 @@
+# fastapi
+
+* 介绍：
+fastapi是最近非常火的python Web框架，通过名字可以看出，它是专注于后端API接口开发的框架。
+
+* 文档：
+https://fastapi.tiangolo.com/zh/
+
+* 运行：
+```shell
+> uvicorn main:app --reload
+INFO:     Uvicorn running on http://127.0.0.1:8000 (Press CTRL+C to quit)
+INFO:     Started reloader process [17208] using statreload
+INFO:     Started server process [22252]
+INFO:     Waiting for application startup.
+INFO:     Application startup complete.
+```
---
+++ fastapi_sample/main.py
@@ -0,0 +1,26 @@
+from typing import Optional
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Optional[bool] = None
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Optional[str] = None):
+    return {"item_id": item_id, "q": q}
+
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}
